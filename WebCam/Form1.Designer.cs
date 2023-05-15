f06296da9219369f625ff76a75e54b0021f310e4
Arioo Barzan <arioobarzan@hotmail.com>
2023-05-15 19:13:21 +0330

    main

---
+++ WebCam/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WebCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.AutoSizeControl = true;
+            this.videoSourcePlayer.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.videoSourcePlayer.ForeColor = System.Drawing.Color.White;
+            this.videoSourcePlayer.Location = new System.Drawing.Point(89, 80);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(322, 242);
+            this.videoSourcePlayer.TabIndex = 2;
+            this.videoSourcePlayer.VideoSource = null;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 380);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 402);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.videoSourcePlayer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WebCam/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+using AForge;
+using AForge.Imaging;
+using AForge.Video;
+using AForge.Video.VFW;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+
+namespace WebCam
+{
+    public partial class Form1 : Form
+    {
+        private IVideoSource videoSource = null;
+        FilterInfoCollection videoDevices;
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        private System.Windows.Forms.Timer alarmTimer = new System.Windows.Forms.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void videoSourcePlayer_NewFrame(object sender, ref Bitmap image)
+        {
+            lock (this)
+            {
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.videoSourcePlayer.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer_NewFrame);
+
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+
+            this.alarmTimer.Interval = 200;
+            this.alarmTimer.Tick += new System.EventHandler(this.alarmTimer_Tick);
+
+            try
+            {
+                // enumerate video devices
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                // add all devices to combo
+                foreach (FilterInfo device in videoDevices)
+                {
+                    comboBox1.Items.Add(device.Name);
+                }
+            }
+            catch (ApplicationException)
+            {
+                comboBox1.Items.Add("No local capture devices");
+                comboBox1.Enabled = false;
+                comboBox1.Enabled = false;
+            }
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(comboBox1.SelectedItem.ToString());
+
+            // open it
+            OpenVideoSource(videoSource);
+        }        // current statistics index
+
+        // statistics length
+        private const int statLength = 15;
+        // current statistics index
+        private int statIndex = 0;
+        // ready statistics values
+        private int statReady = 0;
+        // statistics array
+        private int[] statCount = new int[statLength];
+
+        // counter used for flashing
+        private int flash = 0;
+        private void OpenVideoSource(IVideoSource source)
+        {
+            // set busy cursor
+            this.Cursor = Cursors.WaitCursor;
+
+            // close previous video source
+            CloseVideoSource();
+
+            // start new video source
+            videoSourcePlayer.VideoSource = new AsyncVideoSource(source);
+            videoSourcePlayer.Start();
+
+            // reset statistics
+            statIndex = statReady = 0;
+
+            // start timers
+            timer.Start();
+            alarmTimer.Start();
+
+            videoSource = source;
+
+            this.Cursor = Cursors.Default;
+        }
+        private void CloseVideoSource()
+        {
+            // set busy cursor
+            this.Cursor = Cursors.WaitCursor;
+
+            // stop current video source
+            videoSourcePlayer.SignalToStop();
+
+            // wait 2 seconds until camera stops
+            for (int i = 0; (i < 50) && (videoSourcePlayer.IsRunning); i++)
+            {
+                Thread.Sleep(100);
+            }
+            if (videoSourcePlayer.IsRunning)
+                videoSourcePlayer.Stop();
+
+            // stop timers
+            timer.Stop();
+            alarmTimer.Stop();
+
+            //motionHistory.Clear();
+
+          
+
+            videoSourcePlayer.BorderColor = Color.Black;
+            this.Cursor = Cursors.Default;
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            IVideoSource videoSource = videoSourcePlayer.VideoSource;
+
+            if (videoSource != null)
+            {
+                // get number of frames for the last second
+                statCount[statIndex] = videoSource.FramesReceived;
+
+                // increment indexes
+                if (++statIndex >= statLength)
+                    statIndex = 0;
+                if (statReady < statLength)
+                    statReady++;
+
+                float fps = 0;
+
+                // calculate average value
+                for (int i = 0; i < statReady; i++)
+                {
+                    fps += statCount[i];
+                }
+                fps /= statReady;
+
+                statCount[statIndex] = 0;
+
+                label1.Text = fps.ToString("F2") + " fps";
+            }
+        }
+        private void alarmTimer_Tick(object sender, EventArgs e)
+        {
+            if (flash != 0)
+            {
+                videoSourcePlayer.BorderColor = (flash % 2 == 1) ? Color.Black : Color.Red;
+                flash--;
+            }
+        }
+
+    }
+}
